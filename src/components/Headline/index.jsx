a28b157ec845d8b45628d19ecfc7ee2a48258925
SlashRocky <xxx.good.luck.my.love.xxx@gmail.com>
2021-11-06 19:00:40 +0900

    ✨ コンポーネント間のStateのリフトアップについて

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import classes from "src/components/Headline/Headline.module.css";
 
 export const Headline = (props) => {
-  const { page, children } = props;
+  const { page, children, reduceHandler } = props;
 
   return (
     <div>
       <h1 className={classes.title}>{page} Page</h1>
-      <p className={classes.description}>Get started by editing {children}</p>
+      <p className={classes.description}>アイテムの数は {children} です。</p>
+      <button onClick={reduceHandler}>減らす</button>
     </div>
   );
 };
