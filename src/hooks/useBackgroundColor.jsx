fcb59c2280face87046db14b7a108db569c03950
SlashRocky <xxx.good.luck.my.love.xxx@gmail.com>
2021-11-06 20:02:26 +0900

    ✨ useRouterについて

--- src/hooks/useBackgroundColor.jsx
+++ src/hooks/useBackgroundColor.jsx
@@ -1,11 +1,30 @@
+import { useMemo } from "react";
+import { useRouter } from "next/router";
 import {useEffect} from "react";
 
 export const useBackgroundColor = () => {
+  const router = useRouter();
+  console.log(router);
+
+  const bgColor = useMemo(() => {
+    switch(router.pathname) {
+      case "/": {
+        return "lightblue"
+      }
+      case "/about": {
+        return "beige";
+      }
+      default: {
+        return "";
+      }
+    }
+  }, [router.pathname]);
+
   useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
+    document.body.style.backgroundColor = bgColor;
 
     return () => {
       document.body.style.backgroundColor = "";
     }
-  }, []);
+  }, [bgColor]);
 }
