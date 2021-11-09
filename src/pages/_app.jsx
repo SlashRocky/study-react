9df4e48a05e02848a02c173d6828e69803a95772
SlashRocky <xxx.good.luck.my.love.xxx@gmail.com>
2021-11-09 21:04:13 +0900

    🎨 Layoutをコンポーネントを使ってまとめた

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,12 @@
+import { Layout } from 'src/components/Layout';
 import 'src/styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp
