dc848d649764242483041e1d2e184bfce06f4809
SlashRocky <xxx.good.luck.my.love.xxx@gmail.com>
2021-11-15 00:45:45 +0900

    ⚡️ 初期状態では一部のページだけまずSG化して、それ以外のページでは後でユーザーがリクエストを試みるタイミングでSG化

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,9 +1,10 @@
-import { Header } from "src/components/Header";
-import { Comment } from "src/components/Comment"
 import { SWRConfig } from "swr";
 
+import { Header } from "src/components/Header";
+import { Comment } from "src/components/Comment"
+
 export const getStaticPaths = async () => {
-  const comments = await fetch("https://jsonplaceholder.typicode.com/comments");
+  const comments = await fetch("https://jsonplaceholder.typicode.com/comments?_limit=10");
   const commentsData = await comments.json();
   const paths = commentsData.map((comment) => ({
       params: { id: comment.id.toString() }
@@ -11,7 +12,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: "blocking",
   }
 }
 
@@ -19,6 +20,13 @@ export const getStaticProps = async (ctx) => {
   const { id } = ctx.params;
   const COMMENT_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
   const comment = await fetch(COMMENT_API_URL);
+
+  if (!comment.ok) {
+    return {
+      notFound: true,
+    }
+  }
+
   const commentData = await comment.json();
 
   return {
